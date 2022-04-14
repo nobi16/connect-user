429e8aa52dc7c049863e834702b9d44655652967
nobi16 <ajraichura@gmail.com>
2022-04-14 17:16:45 +0530

    final

--- connect-u/src/components/Home.jsx
+++ connect-u/src/components/Home.jsx
@@ -78,6 +78,7 @@ function Home({ businessData, fetchbusiness, search }) {
 		}
 
 	}, [businessData, search]);
+	
 	useEffect(() => {
         // if (product) {
           let pcount = Math.ceil(filterDatas.length / perPage);
